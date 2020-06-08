f699a07968ecd137bb7adc67db69405d60b6eaa2
Chad Tolleson <tolleson@pdx.edu>
2020-06-07 22:11:23 -0700

    added glossary and flag the succcessful players

--- SVM.py
+++ SVM.py
@@ -6,29 +6,28 @@ import numpy as np
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-
 def loadData():
     """load the source data"""
-    
-    # load source data from the source files
     playerData = pd.read_csv(os.path.join(__location__,'./SourceData/player_data.csv'))
     players = pd.read_csv(os.path.join(__location__,'./SourceData/Players.csv'))
-    seasonsStats = pd.read_csv(os.path.join(__location__,'./SourceData/Seasons_Stats.csv'))
-    glossary = pd.read_csv(os.path.join(__location__,'./SourceData/Seasons_Stats_Glossary.txt'),
-                           sep='|', names=['abbv','description'], skip_blank_lines=True)
+    seasonsStats = pd.read_csv(os.path.join(__location__,'./SourceData/Seasons_Stats.csv'), index_col=0)
+    glossary = dict(pd.read_csv(os.path.join(__location__,'./SourceData/Seasons_Stats_Glossary.txt'),
+                           sep='|', names=['abbv','description'], skip_blank_lines=True).values)
 
-    if (1):
-        # print(f"playerData shape:{playerData.shape}")
-        # print(f"players shape:{players.shape}")
-        # print(f"seasonsStats shape:{seasonsStats.shape}")
-        # print(playerData)
-        # print(players)
-        # print(seasonsStats)
+    if (v):
+        print(f"playerData shape:{playerData.shape}")
+        print(f"players shape:{players.shape}")
+        print(f"seasonsStats shape:{seasonsStats.shape}")
+    if (v):
+        print(playerData)
+        print(players)
+        print(seasonsStats)
         print(glossary)
+        # seasonsStats.rename(columns = glossary, inplace=True)
 
-    return playerData, players, seasonsStats
+    return playerData, players, seasonsStats, glossary
 
-def findSuccessfulPlayers(seasonsStats, ng):
+def idSuccessOld(seasonsStats, ng):
     playerGames={}
     success_players=[]
     if (v): print(seasonsStats['G'].head())
@@ -41,12 +40,22 @@ def findSuccessfulPlayers(seasonsStats, ng):
           if (v): print(player, playerGames[player])
           success_players.append(player)
 
+def idSuccessNew(seasonsStats, ng):
+    successP = seasonsStats.groupby(['Player'])['G'].sum()
+    successP.columns = ['Player', 'G']
+    successP = successP >= ng
+    if (v): print(successP)
+    return successP
+
 if __name__ == "__main__":
     timeStart = time.time()
     v = 0 # flag for verbose printing
     ng = 174 # number of games played needed to be a "successful" player
-    playerData, players, seasonsStats = loadData()
-    # success_players = findSuccessfulPlayers(seasonsStats, ng)
+    playerData, players, seasonsStats, glossary = loadData()
+    if 1:
+        success_players = idSuccessNew(seasonsStats, ng)
+    else:
+        success_players = idSuccessOld(seasonsStats, ng)
     
 
     timeEnd = time.time()
