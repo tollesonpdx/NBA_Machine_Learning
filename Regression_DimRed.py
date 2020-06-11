ec4ac52cce6cf16353818cb148612dd5e4c8a68b
Tom <40807906+tlan2@users.noreply.github.com>
2020-06-11 15:19:29 -0700

    Graph works but wrong orientation. Recursive Feature Elimination done.

--- Regression_DimRed.py
+++ Regression_DimRed.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-nu
 import os
 import sys
 import time
@@ -11,6 +11,7 @@ from sklearn.utils import shuffle
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import RFE
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning) 
 from sklearn.metrics import mean_squared_error, r2_score
@@ -26,20 +27,20 @@ def timestamp(start):
 def plot_confmat(cm):
 
     df_cm = pd.DataFrame(cm, range(2), range(2))
-    # plt.figure(figsize=(10,7))
+    plt.figure(figsize=(10,7))
     sn.set(font_scale=1.4) # for label size
     sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}) # font size
     plt.title(f'Baller or No Baller?: | test set confusion matrix')
     plt.xlabel('Actual')
     plt.ylabel('Predicted')
-    plt.show()
+    # plt.show()
 
 def prep_data(source, split):
 
-  source=source.iloc[:,:-1]
+  source=source.iloc[:,:-1] # Removes empty column
   lenTraining=int(len(source) * split)  
   denomanoms = source.abs().max()
-  denomanoms[-1], denomanoms[-2]=1., 1.
+  denomanoms[-1], denomanoms[-2]=1., 1. # Success column & games column removed
   source/=denomanoms
   source.loc[source['nba_gms_plyed']>=200, 'success']=1
   source.loc[source['nba_gms_plyed'] <200, 'success']=0
@@ -49,23 +50,57 @@ def prep_data(source, split):
 
   return trainingData, testingData
 
-def regression(trainData, testData):
+def regression(trainData, testData, tally):
   
   confmat=np.zeros((2,2))
   
-  X_train=trainData.drop(columns=['success']) 
-  X_test =testData.drop(columns=['success']) 
+  X_train=trainData.drop(columns=['draft_yr','rpg', 'ast','success']) # Drop draft pick?
+  X_test =testData.drop(columns=['draft_yr','rpg', 'ast','success']) # Drop draft pick?
   y_train=trainData['success'].values
   y_test =testData ['success'].values
   
-  lm = LogisticRegression(random_state = 0)
-  lm.fit(X_train, y_train)
-  pred=lm.predict(X_test)
+  # lm = LogisticRegression(random_state = 0)
+  # lm.fit(X_train, y_train)
+  # pred=lm.predict(X_test)
+
+  # Use this as my base code - https://machinelearningmastery.com/feature-selection-machine-learning-python/
+
+  model = LogisticRegression(solver='lbfgs') # lbfgs-Stands for Limited-memory Broyden–Fletcher–Goldfarb–Shanno
+  #nFeatures = 5
+  columns = X_train.columns
+  #print(len(columns))
+  #tally = np.zeros(len(columns))
+  #print(f'tally size: {len(tally)}')
+  for i in range(1,len(columns)):
+    nFeatures = i
+    rfe = RFE(model, nFeatures)
+    fit = rfe.fit(X_train, y_train)
+  #print("\nNum Features: %d" % fit.n_features_)
+  # print(X_train.columns)
+    #print(len(X_train.columns))
+  # print(len(fit.support_))
+    bools = fit.support_
+    print(i)
+    print(bools)
+    #print(f"=== Top {nFeatures} Features ===")
+    for i in range(len(columns)):
+      if (bools[i] == True):
+        tally[i] += 1
+        #print(columns[i])
+  #print()
+  #print("Selected Features: %s" % fit.support_)
+  #print("Feature Ranking: %s" % fit.ranking_)
+  
+  pred = rfe.predict(X_test)
+ 
+  
   correct=0
   for i in range(len(pred)):
     if y_test[i]==pred[i]:
       correct+=1
     confmat[int(pred[i])][int(y_test[i])]+=1
+  
+  #print(confmat)
 
   # print(f'Percent correctly predicted by logistic regression model: {round(correct/len(y_test), 2)}%')
   # print('Coefficients: \n', lm.coef_)
@@ -90,40 +125,77 @@ def regression(trainData, testData):
     plt.tight_layout()   
     plt.show()
 
-  return correct/len(y_test), confmat
+  return correct/len(y_test), confmat, tally
+
+def displayGraph(trainData,pctTally):
+    #fig = plt.figure()
+    colors = ['brown','deepskyblue','orangered','darkblue','green','gray',\
+              'lightblue', 'rosybrown', 'orange', 'pink', 'black','red',\
+              'crimson','goldenrod', 'yellow', 'blue', 'salmon', 'aquamarine',\
+              'dimgray', 'tan', 'hotpink','purple']  # 'tomato','lawngreen', 'darkturquoise','darkorange'
+   
+    trainData = trainData.drop(columns=['draft_yr','rpg','ast','success'])
+    columns = trainData.columns
+    dataTuples = list(zip(columns, pctTally))
+    # print(dataTuples)
+    df = pd.DataFrame(dataTuples, columns=['feature', 'pct'])
+    df.sort_values(by=['pct'], inplace=True, ascending=False)
+    print(df)
+    # print(f'# of pctTally: {len(pctTally)}')
+    # print(f'# of colors: {len(colors)}')
+    # print(f'# of columns: {len(columns)}')
+    # print(columns)
+    nColumns = np.arange(len(columns))
+    plt.title('Reduced Models: Feature Appearance Frequency')
+    plt.ylabel('Percentage')
+    plt.xticks(nColumns, columns, rotation=90, size=8)
+    #plt.grid(color='#95a5a6', linestyle='--', linewidth=0.75, axis='y', alpha=0.5)
+    plt.bar(columns,pctTally,color=colors, align='center')
+    plt.tight_layout()
+    plt.show()
 
 def main():
   timeStart = time.time()
-  split=.8
+  split=.8 #80/20 split similar to paper
   toms_data = pd.read_csv(os.path.join(__location__,'./SourceData/fixed_nans.csv'))
-  scores=np.zeros(100)
-  
-  for i in range(100):
+  scores=np.zeros(10)
+  tally = np.zeros(22)
+  for i in range(1):
     trainData,testData=prep_data(toms_data, split)
-    scores[i],confmat=regression(trainData, testData)
+    scores[i],confmat,tally=regression(trainData, testData, tally)
   # print(scores)
+  print("\n=== Tally ===")
+  pct_tally = [round((x / max(tally))*100,2) for x in tally]
+  print(tally)
+  print(trainData.columns)
+  print(pct_tally)
+  print()
   TP,FP,FN,TN=confmat[0][0],confmat[0][1],confmat[1][0],confmat[1][1]
-  accuracy =(TP+TN)/(TP+FP+FN+TN)
-  precision=TP/(TP+FP)
-  recall   =TP/(TP+FN)  
-  print(f'accuracy  = {accuracy} ')
-  print(f'precision = {precision}')
-  print(f'recall    = {recall}   ')
-  img =plt.imread("court.jpg")
-  plt.xlabel('Trial', fontsize=20)
-  plt.ylabel('Percent of Correct Predictions', fontsize=20)
-  plt.title('Overall Predictive Percent: Regression', fontsize=20)
-  plt.imshow(img, zorder=0, extent=[-5,100, 30,100])
-  avg=sum(scores*100)/len(scores)
-  avgs=[avg for i in range(len(scores))]
-  red_patch = mpatches.Patch(color='red', label='Avg: {:6.2f}%'.format(avg))
-  black_patch = mpatches.Patch(color='black', label='Individual Trial')
-  plt.legend(handles=[red_patch, black_patch])
-  plt.scatter(np.arange(len(scores)), avgs, s=3, c='red', zorder=1)
-  plt.scatter(np.arange(len(scores)), scores*100, c='black', zorder=1)
-  plt.show()
-  plot_confmat(confmat)
-
+  accuracy = round((TP+TN)/(TP+FP+FN+TN),3) * 100
+  # precision=TP/(TP+FP)
+  # recall   =TP/(TP+FN)  
+  print(f'accuracy  = {accuracy}%')
+  # print(f'precision = {precision}')
+  # print(f'recall    = {recall}   ')
   timestamp(timeStart)
+  displayGraph(trainData, pct_tally)
+
+
+  # img =plt.imread("court.jpg")
+  # plt.xlabel('Trial', fontsize=20)
+  # plt.ylabel('Percent of Correct Predictions', fontsize=20)
+  # plt.title('Overall Predictive Percent: Regression', fontsize=20)
+  # plt.imshow(img, zorder=0, extent=[-5,100, 30,100])
+  # avg=sum(scores*100)/len(scores)
+  # avgs=[avg for i in range(len(scores))]
+  # red_patch = mpatches.Patch(color='red', label='Avg: {:6.2f}%'.format(avg))
+  # black_patch = mpatches.Patch(color='black', label='Individual Trial')
+  # plt.legend(handles=[red_patch, black_patch])
+  # plt.scatter(np.arange(len(scores)), avgs, s=3, c='red', zorder=1)
+  # plt.scatter(np.arange(len(scores)), scores*100, c='black', zorder=1)
+  # # plt.show()
+  # plot_confmat(confmat)
+
+  
 
 main()
